c29564241a6d4d8139a11f2c7a3bcb958b021ee1
mobish26 <96426964+mobish26@users.noreply.github.com>
2022-01-15 19:18:31 +0100

    index.js nya funk implementerat
    kunna se element från local storage

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ addBtn.addEventListener("click", function(e) {
 
 	showNotes();
 });
+
+// Function to show elements from localStorage
+function showNotes() {
+	let notes = localStorage.getItem("notes");
+
+	if (notes == null) notesObj = [];
+	else notesObj = JSON.parse(notes);
+
+	let html = "";
+
+	notesObj.forEach(function(element, index) {
+		html += `<div class="noteCard my-2 mx-2 card"
+			style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">
+						Note ${index + 1}
+					</h5>
+					<p class="card-text">
+						${element}
+					</p>
+
+				<button id="${index}" onclick=
+					"deleteNote(this.id)"
+					class="btn btn-primary">
+					Delete Note
+				</button>
+			</div>
+		</div>`;
+	});
